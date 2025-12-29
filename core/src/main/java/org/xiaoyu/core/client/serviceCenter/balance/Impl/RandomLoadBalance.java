a58baf617d9864b9526b3a56002dd16febecb582
xiaoyu <3344609717@qq.com>
2025-12-29 21:17:25 +0800

    remove log factory

--- core/src/main/java/org/xiaoyu/core/client/serviceCenter/balance/Impl/RandomLoadBalance.java
+++ core/src/main/java/org/xiaoyu/core/client/serviceCenter/balance/Impl/RandomLoadBalance.java
@@ -1,7 +1,6 @@
 package org.xiaoyu.core.client.serviceCenter.balance.Impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.xiaoyu.core.client.serviceCenter.balance.LoadBalance;
 
 import java.util.List;
@@ -11,9 +10,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * 随机数负载均衡
  */
+@Slf4j
 public class RandomLoadBalance implements LoadBalance {
     private final static Random random = new Random();
-    private static final Logger log = LoggerFactory.getLogger(RandomLoadBalance.class);
 
     private final List<String> addressList = new CopyOnWriteArrayList<>();
 
